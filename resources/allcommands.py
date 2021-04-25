4e7c306e0430783cdc5f7903a5dc52d28e73c1e3
Ankith Abhayan <ankithabhayan@gmail.com>
2021-04-25 10:50:43 +0100

    rewriting whole bot #15

--- resources/allcommands.py
+++ resources/allcommands.py
@@ -21,10 +21,10 @@ async def sayhello(ctx,*args):
 
 async def pingedunnecessary(client,message):
     if not message.author.has_role("Owner"):
-        embed=discord.Embed(title="Dont do it",description=str(ctx.author.mention)+" please dont try to ping everyone",color=0x0066ff)
+        embed=discord.Embed(title="Dont do it",description=str(message.author.mention)+" please dont try to ping everyone",color=0x0066ff)
         await message.channel.send(embed=embed)
         channel = client.get_channel(notice_channel_id)
-        embed=discord.Embed(title="Notice: **Pinged Everyone**",description="**"+str(message.author.mention)+"** has tried to ping everyone \n **channel**: "+str(message.channel)+"\n **full ctx**: "+str(message.content)+"\n **time**: "+str(date_time.date())+" "+str(date_time.time()),color=0x0066ff) 
+        embed=discord.Embed(title="Notice: **Pinged Everyone**",description="**"+str(message.author.mention)+"** has tried to ping everyone \n **channel**: "+str(message.channel)+"\n **full message**: "+str(message.content)+"\n **time**: "+str(date_time.date())+" "+str(date_time.time()),color=0x0066ff) 
         await channel.send(embed=embed)
 
 @commands.command(name='sendch')
@@ -138,7 +138,8 @@ async def unmute(ctx,*args):
         await ctx.channel.send(embed=embed)
 
 async def filtermessage(ctx,bot):
-    if "@everyone" in ctx.message.content:
+    if "@everyone" in ctx.content:
+        #ctx.channel.send("are you trying to do something dude?")
         await pingedunnecessary(ctx,bot)
     for item in ["fuck","bitch","cumshot","asshole","wtf","retard","cocksucker","nigger","sex"]:
         if item in ctx.message.content:
@@ -146,7 +147,7 @@ async def filtermessage(ctx,bot):
                 pass
             else:
                 channel = bot.get_channel(notice_channel_id)
-                embed=discord.Embed(title="Notice: **language breach**",description="**"+str(ctx.author)+"** sent a bad word in this server\n **textchannel**: "+str(ctx.channel)+"\n **full ctx**: "+str(ctx.message.content)+"\n**date and time**: "+str(date_time.time())+" "+str(date_time.date()),color=0x0066ff) 
+                embed=discord.Embed(title="Notice: **language breach**",description="**"+str(ctx.author)+"** sent a bad word in this server\n **textchannel**: "+str(ctx.channel)+"\n **full message**: "+str(ctx.message.content)+"\n**date and time**: "+str(date_time.time())+" "+str(date_time.date()),color=0x0066ff) 
                 await channel.send(embed=embed)
 
 @commands.command(name='help')
@@ -182,7 +183,7 @@ async def rules(ctx,*args):
     with open("resources/data.json","r") as JsonFile:
         data = json.load(JsonFile)
     string = data["rules"]
-    rulesfull = data["rulesfull"]
+    rulesfull message = data["rulesfull message"]
     rulesfull = rulesfull.split("|")
     print(rulesfull)
     JsonFile.close()
