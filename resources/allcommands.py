0e010d1fee4f80911af4a864ba0db96f4c39f0cf
Ankith Abhayan <ankithabhayan@gmail.com>
2021-04-25 10:45:35 +0100

    rewriting whole bot #14

--- resources/allcommands.py
+++ resources/allcommands.py
@@ -30,13 +30,13 @@ async def pingedunnecessary(client,message):
 @commands.command(name='sendch')
 async def sendch(ctx,*args):
     if "Admin" in str(ctx.author.roles):
-        array = ctx.content.split()
+        array = ctx.message.content.split()
         channel = ctx.get_channel(int(array[1]))
         await channel.send(' '.join(array[2:len(array)]))
 
 @commands.command(name='kick')
 async def kick(ctx,*args):
-    array = ctx.content.split()
+    array = ctx.message.content.split()
     if "Admin" in str(ctx.author.roles):
         victim = array[1]
         if '<@!' in victim:
@@ -60,7 +60,7 @@ async def kick(ctx,*args):
 
 @commands.command(name='remove_role')
 async def remove_role(ctx,*args):
-    array = ctx.content.split()
+    array = ctx.message.content.split()
     if "Owner" in str(ctx.author.roles):
         user = await ctx.guild.fetch_member(int(array[2]))
         role = discord.utils.get(ctx.guild.roles, name=array[1])
@@ -77,7 +77,7 @@ async def remove_role(ctx,*args):
 
 @commands.command(name='add_role')
 async def add_role(ctx,*args):
-    array = ctx.content.split()
+    array = ctx.message.content.split()
     if "Owner" in str(ctx.author.roles):
         user = await ctx.guild.fetch_member(int(array[2]))
         role = discord.utils.get(ctx.guild.roles, name=array[1])
@@ -95,7 +95,7 @@ async def add_role(ctx,*args):
 @commands.command(name='mute')
 async def mute(ctx,*args):
     if "Admin" in str(ctx.author.roles):
-        array = ctx.content.split()
+        array = ctx.message.content.split()
         victim = array[1]
         if '<@!' in victim:
             victim = re.search('\d+',victim).group()
@@ -121,7 +121,7 @@ async def mute(ctx,*args):
 @commands.command(name='unmute')
 async def unmute(ctx,*args):
     if "Admin" in str(ctx.author.roles):
-        array = ctx.content.split()
+        array = ctx.message.content.split()
         victim = array[1]
         if '<@!' in victim:
             victim = re.search('\d+',victim).group()
@@ -138,15 +138,15 @@ async def unmute(ctx,*args):
         await ctx.channel.send(embed=embed)
 
 async def filtermessage(ctx,bot):
-    if "@everyone" in ctx.content:
+    if "@everyone" in ctx.message.content:
         await pingedunnecessary(ctx,bot)
     for item in ["fuck","bitch","cumshot","asshole","wtf","retard","cocksucker","nigger","sex"]:
-        if item in ctx.content:
+        if item in ctx.message.content:
             if "Admin" in str(ctx.author.roles):
                 pass
             else:
                 channel = bot.get_channel(notice_channel_id)
-                embed=discord.Embed(title="Notice: **language breach**",description="**"+str(ctx.author)+"** sent a bad word in this server\n **textchannel**: "+str(ctx.channel)+"\n **full ctx**: "+str(ctx.content)+"\n**date and time**: "+str(date_time.time())+" "+str(date_time.date()),color=0x0066ff) 
+                embed=discord.Embed(title="Notice: **language breach**",description="**"+str(ctx.author)+"** sent a bad word in this server\n **textchannel**: "+str(ctx.channel)+"\n **full ctx**: "+str(ctx.message.content)+"\n**date and time**: "+str(date_time.time())+" "+str(date_time.date()),color=0x0066ff) 
                 await channel.send(embed=embed)
 
 @commands.command(name='help')
@@ -157,14 +157,14 @@ async def showhelp(ctx,*args):
     all_commands_false = data["all_commands_false"]
     helpinfo = data["helpinfo"]
     JsonFile.close()
-    if len(ctx.content.split()) == 1:
+    if len(ctx.message.content.split()) == 1:
         embed=discord.Embed(title="$modbot", description="$modbot is a bot for moderators made by the developer team at "+str(ctx.guild)+" here are all commands:", color=0x0066ff)
         for key,value in helpinfo.items():
             embed.add_field(name=key, value=value,inline=False)
         embed.set_footer(text="developed by ankith101.rar")
         await ctx.channel.send(embed=embed)
     else:
-        command = ctx.content.split()[1]
+        command = ctx.message.content.split()[1]
         if command in all_commands:
             embed=discord.Embed(title=command,description=helpinfo[command],color=0x0066ff)
             embed.set_footer(text="developed by ankith101.rar")
@@ -186,7 +186,7 @@ async def rules(ctx,*args):
     rulesfull = rulesfull.split("|")
     print(rulesfull)
     JsonFile.close()
-    array = ctx.content.split()
+    array = ctx.message.content.split()
     if len(array) == 1:
         embed=discord.Embed(title="Rules of the server:",url="https://htmlpreview.github.io/?https://github.com/AnkithAbhayan/modbot-discord/blob/main/docs/rules.html",description="You can get a copy of the server rules from our website\n [click here](https://htmlpreview.github.io/?https://github.com/AnkithAbhayan/modbot-discord/blob/main/docs/rules.html) to view the rules",color=0x0066ff)
         embed.set_footer(text="website by Aadi, bot developed by Ankith101")
@@ -225,7 +225,7 @@ async def unsilence(ctx,*args):
 @commands.command(name='warn')
 async def warn(ctx,*args):
     if "Admin" in str(ctx.author.roles):
-        array = ctx.content.split()
+        array = ctx.message.content.split()
         user_id = array[1]
         reason = " ".join(array[2:len(array)])
         user = await ctx.guild.fetch_member(int(array[1]))
