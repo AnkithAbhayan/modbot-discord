c64d517b3d902e8d2896bfbcb072d4cdce7506f5
Ankith Abhayan <ankithabhayan@gmail.com>
2021-04-27 14:27:29 +0100

    rewriting whole bot #42

--- resources/allcommands.py
+++ resources/allcommands.py
@@ -265,7 +265,7 @@ class self_assign_roles(commands.Cog):
         all_roles_string = ' '.join(data['user_roles'])
         if len(array) == 1:
             help_get_role = discord.Embed(title="Command help: `$get_role`",description=f"This command lets you self assign roles to your user so that you can access the full benefits!\nUsage:\n```\n$get_role <role_name>```\n\n Example: `$get_role developer` gives you access to all programming related channels!\n All self-assignable roles: {all_roles_string}")
-            ctx.channel.send(embed=help_get_role)
+            await ctx.channel.send(embed=help_get_role)
             return
         developer_role = discord.utils.get(ctx.guild.roles, name="developer")
         if developer_role not in user.roles:
@@ -276,8 +276,8 @@ class self_assign_roles(commands.Cog):
             return
         if array[1] in data["user_roles"]:
             role = discord.utils.get(ctx.guild.roles, name=array[1])
-            ctx.author.add_roles(role)
+            await ctx.author.add_roles(role)
             got_the_role_embed = discord.Embed(title=f"@{array[1]} role added!",description=f"You have now got the @{array[1]} role! :tada:",color=0x0066ff)
         else:
             error_in_role_name = discord.Embed(title="Invalid role.",description=f"The role name you entered is incorrect!\n Correct usage:\n ```\n$get_role <role_name>\n```\nHere are all the valid role names: {all_roles_string}",color=0x0066ff)
-            ctx.channel.send(embed=error_in_role_name)
+            await ctx.channel.send(embed=error_in_role_name)
