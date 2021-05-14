7cb006d592c2a2c6a40bb23b6b206fa1461e8b2c
Ankith Abhayan <ankithabhayan@gmail.com>
2021-05-14 13:41:05 +0100

    ok done rewriting ig

--- bot/exts/misc/help.py
+++ bot/exts/misc/help.py
@@ -13,37 +13,50 @@ class Help(commands.Cog):
         all_commands_false = constants.json_data["all_commands_false"]
         helpinfo = constants.json_data["helpinfo"]
         if len(ctx.message.content.split()) == 1:
+            categories = [category for category in helpinfo]
             embed = discord.Embed(
-                title="$modbot",
-                description=f"$modbot is a bot for moderators made by the developer team at {ctx.guild} here are all commands:",
+                title="Command Help.",
+                description=(
+                    f"$modbot is a bot for moderators made by the developer team at {ctx.guild}\n"
+                    "If you'd like to view command help for a different category:\n"
+                    "```\n"
+                    "$help <category>\n"
+                    "```\n\n"
+                    f"**All the categories**: `{', '.join(categories)}`"
+                    f"If you're just looking for help info on a specific command:\n"
+                    "```\n"
+                    "$help <command>\n"
+                    "```"
+                )
                 color=constants.colours["blue"],
             )
-            for key, value in helpinfo.items():
-                embed.add_field(name=key, value=value, inline=False)
             embed.set_footer(text="developed by ankith101.rar")
             await ctx.channel.send(embed=embed)
         else:
-            command = ctx.message.content.split()[1]
-            if command in all_commands:
+            second_arg = ctx.message.content.split()[1]
+            if helpinfo.get(second_arg):
                 embed = discord.Embed(
-                    title=command,
-                    description=helpinfo[command],
+                    title=f"Command help. Category: {second_arg}",
+                    description=f"Here are all the commands of the `{second_arg}` category.",
                     color=constants.colours["blue"],
                 )
-                embed.set_footer(text="developed by ankith101.rar")
-                await ctx.channel.send(embed=embed)
-            elif command in all_commands_false:
-                embed = discord.Embed(
-                    title="$" + command,
-                    description=helpinfo["$" + command],
-                    color=constants.colours["blue"],
-                )
+                for commandname, info in helpinfo[second_arg].items():
+                    embed.add_field(name=f"${commandname}", value=info, inline=False)
                 embed.set_footer(text="developed by ankith101.rar")
                 await ctx.channel.send(embed=embed)
             else:
+                for category, commands in helpinfo.items():
+                    if commands.get(second_arg):
+                        embed = discord.Embed(
+                            title=f"Command Help: `${command}`",
+                            description=helpinfo[category][second_arg],
+                            color=constants.colours["blue"],
+                        )
+                        await ctx.channel.send(embed=embed)
+                        return
                 embed = discord.Embed(
-                    title=f"Unknown command: `{command}`",
-                    description="idk what you typed, check the spelling",
+                    title=f"Unknown command or Category: `{command}`",
+                    description=f"`{command}` is not a valid command name or a category.",
                     color=constants.colours["red"],
                 )
                 await ctx.channel.send(embed=embed)
