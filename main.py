992dc0c7e4ced4b17edb9187a6e7274404d74c20
Ankith Abhayan <ankithabhayan@gmail.com>
2021-05-16 13:39:38 +0100

    ok

--- main.py
+++ main.py
@@ -74,8 +74,11 @@ async def on_message(message):
         if message.author == bot.user:
             my_embed = discord.Embed(
                 title="Message successfully sent :white_check_mark:",
-                description=f"`Receiver`: `{message.channel}`\n`time`: `{datetime.now()}`",
+                description=f"`time`: `{datetime.now()}`",
                 color=constants.colours["blue"]
+            )
+            my_embed.set_author(
+                name=f"`Sent via`: `{message.channel}`", 
             )
             my_embed.set_footer(text="dm message command.")
             channel = bot.get_channel(constants.channels["bot-testing"])
