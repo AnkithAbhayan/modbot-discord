4fc480a1bcd4b4f2f6f6d06961eb951084a3973b
Ankith Abhayan <ankithabhayan@gmail.com>
2021-05-16 13:17:09 +0100

    ok

--- bot/exts/moderation/senddm.py
+++ bot/exts/moderation/senddm.py
@@ -33,7 +33,7 @@ class SendDm(commands.Cog):
             roles = ctx.author.roles #list of roles, lowest role first
             roles.reverse() #list of roles, highest role first
             top_role = roles[0]
-            dm_message.set_footer(text=f"{top_role.name} • {datetime.today().strftime('%d-%m-%y')}")
+            dm_message.set_footer(text=f"{top_role.name} • {datetime.today().strftime('%d/%m/%yyyy')}")
             await user.dm_channel.send(embed=dm_message)
         else:
             embed = discord.Embed(
